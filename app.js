6363da774188cc8f31f099f478b575d454094179
Isaia26633 <123019005+Isaia26633@users.noreply.github.com>
2025-02-12 16:25:18 -0500

    reworked backend

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Import required modules
 const express = require("express");
 const ejs = require("ejs");
 const sqlite3 = require("sqlite3").verbose();
@@ -6,14 +5,12 @@ const path = require("path");
 const jwt = require('jsonwebtoken');
 const session = require('express-session');
 
-// Import custom modules
 const routes = require("./modules/routes.js");
 const { isAuthenticated, AUTH_URL, THIS_URL } = require("./modules/authentication.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Create a new database, set as the "db" object
 const db = new sqlite3.Database("db/database.db", (err) => {
     if (err) {
         console.error("Failed to connect to the database: ", err);
@@ -21,10 +18,8 @@ const db = new sqlite3.Database("db/database.db", (err) => {
     }
 });
 
-// Set view engine
 app.set("view engine", "ejs");
 
-// Use middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session({
@@ -32,26 +27,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
-
-app.get('/', isAuthenticated, (req, res) => {
-    try {
-        res.render('index.ejs', { user: req.session.user });
-    } catch (error) {
-        res.send(error.message);
-    }
-});
-
-app.get('/login', (req, res) => {
-    if (req.query.token) {
-        let tokenData = jwt.decode(req.query.token);
-        req.session.token = tokenData;
-        req.session.user = tokenData.username;
-        username = req.session.user;
-        res.redirect('/');
-    } else {
-        res.render('login.ejs', { AUTH_URL, THIS_URL });
-    }
-});
+app.use('/', routes);
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
--- modules/routes.js
+++ modules/routes.js
@@ -1,16 +1,40 @@
-/*----------
-GET Requests
-----------*/
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { isAuthenticated, AUTH_URL, THIS_URL } = require('./authentication.js');
 
-//create root function
 const index = (req, res) => {
-    res.render("index");
+    try {
+        if (req.session.permissions < 5) {
+            res.render('index.ejs', { user: req.session.user });
+        } else if (req.session.permissions >= 5) {
+            res.render('teacher.ejs', { user: req.session.user });
+        }
+    } catch (error) {
+        res.send(error.message);
+    }
 };
 
 const login = (req, res) => {
-    res.render("login");
+    if (req.query.token) {
+        let tokenData = jwt.decode(req.query.token);
+        req.session.token = tokenData;
+        req.session.user = tokenData.username;
+        req.session.permissions = tokenData.permissions;
+        username = tokenData.username;
+        res.redirect('/');
+    } else {
+        res.render('login.ejs', { AUTH_URL, THIS_URL });
+    }
 };
 
-module.exports = {
-    index
+const logout = (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 };
+
+router.get('/', isAuthenticated, index);
+router.get('/login', login);
+router.get('/logout', logout);
+
+module.exports = router;
