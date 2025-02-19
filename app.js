e5809fd6e1c31d0987961bc3369b56fb87acec84
Isaiah Knaby <123019005+Isaia26633@users.noreply.github.com>
2025-02-19 08:17:33 -0800

    added permissions

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session');
 
 const routes = require("./modules/routes.js");
-const { isAuthenticated, AUTH_URL, THIS_URL } = require("./modules/authentication.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,11 +22,62 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session({
-    secret: 'make up a secret string here but never publish it!',
+    secret: 'ThisIsTheSuperSigmerSecretKeyThatIsUsedToSignTheSessionIDNobodyWillEverGuessThisCauseItsSoLongAndImSigmaNowHeresARandomStringOfNumbersToMakeItEvenLonger123456789093784983749837498374987234987264928734629874629837612893746219873461298476123847962348976123487965213489762348972314875234987123648923714698123468231746198472364981723649812734698127346981723649',
     resave: false,
     saveUninitialized: false
 }));
+
 app.use('/', routes);
+
+app.get('/login', (req, res) => {
+    if (req.query.token) {
+        let tokenData = jwt.decode(req.query.token);
+        req.session.token = tokenData;
+        let username = tokenData.username;
+        let permissions = tokenData.permissions;
+        db.get('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+            if (err) {
+                console.error("Failed to query the database: ", err);
+                res.status(500).send("Internal Server Error");
+                return;
+            }
+            if (!row) {
+                console.log("User not found in database, inserting...");
+                db.run('INSERT INTO users (username, displayName) VALUES (?, ?)', [username, displayName], (err) => {
+                    if (err) {
+                        console.error("Failed to insert user into database: ", err);
+                    } else {
+                        db.run('INSERT INTO classusers (permissions) VALUES (?)', [permissions], (err) => {
+                            if (err) {
+                                console.error("Failed to insert user into classusers: ", err);
+                            } else {
+                                req.session.user = username;
+                                req.session.permissions = permissions;
+                                res.redirect('/');
+                                console.log("User inserted into database successfully!");
+                            }
+                        });
+                    }
+                });
+            } else {
+                req.session.user = username;
+                req.session.permissions = permissions;
+                res.redirect('/');
+            }
+        });
+
+    } else {
+        res.redirect('https://formbar.yorktechapps.com/oauth?redirectURL=http://localhost:3000/login');
+    }
+});
+
+app.get('/getuserpermissions'), (req, res) => {
+    if (req.session.permissions) {
+        res.send(req.session.permissions);
+    } else {
+        res.status(401).send("Unauthorized");
+    }
+};
 
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
