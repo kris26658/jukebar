e7fb6c38dcd3628c1758857cc36fc2385392ebd1
Kris Bowman <129105342+kris26658@users.noreply.github.com>
2025-04-08 14:12:28 -0400

    Updated authentication

--- modules/authentication.js
+++ modules/authentication.js
@@ -1,23 +1,12 @@
-const AUTH_URL = 'http://localhost:420/oauth';
+const AUTH_URL = 'http://formbeta.yorktechapps.com/oauth';
 const THIS_URL = 'http://localhost:3000/login';
 
 function isAuthenticated(req, res, next) {
     if (req.session.user) {
-        const tokenData = req.session.token;
-
-        try {
-            // Check if the token has expired
-            const currentTime = Math.floor(Date.now() / 1000);
-            if (tokenData.exp < currentTime) {
-                throw new Error('Token has expired');
-            }
-
-            next();
-        } catch (err) {
-            res.redirect(`${FBJS_URL}/oauth?refreshToken=${tokenData.refreshToken}&redirectURL=${THIS_URL}`);
-        }
-    } else {
-        res.redirect(`/login?redirectURL=${THIS_URL}`);
+        next();
+    }
+    else {
+        res.redirect(`${AUTH_URL}?redirectURL=${THIS_URL}`);
     }
 }
 
